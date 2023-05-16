655465eeaa76d4a71ab823a24c37d44c4d6ab9b0
Lucas Gutierrez <lucas.gutierrez@madmobile.com>
2023-05-16 11:24:42 -0300

    favicon

--- app/pages/_document.tsx
+++ app/pages/_document.tsx
@@ -4,7 +4,7 @@ class MyDocument extends Document {
     return (
       <Html>
         <Head>
-          <link rel="icon" href="/favicon.ico" />
+          <link rel="shortcut icon" href="/favicon.ico" />
           <meta name="msapplication-TileColor" content="#0d0d0d" />
           <meta name="theme-color" content="#0d0d0d" />
           <meta name="viewport" content="width=device-width, initial-scale=1, minimum-scale=1" />
